fa96d78a1430e423d6aafc1104a5530d2a0bbb86
iam-micah <micahnettey@gmail.com>
2023-11-20 04:01:06 -0500

    create homepage and navbar

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-    return <div>Hello</div>;
+    return (
+        <div>
+            <Home />
+            <Navbar />
+        </div>
+    );
 }
 
 export default App;
---
+++ blog/src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <h1>Welcome to my simple Blog page</h1>
+            <p>
+                I just want to write out things about my life. Think of this
+                like an online journal for documenting my thoughts, and events
+                in my life
+            </p>
+        </div>
+    );
+};
+
+export default Home;
