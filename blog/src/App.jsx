af09358b307266af42338a70c193d5d428c03222
iam-micah <micahnettey@gmail.com>
2023-11-20 04:21:03 -0500

    create posts and navbar code

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,13 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
+import Posts from "./components/Posts/Posts";
 
 function App() {
     return (
-        <div>
-            <Home />
-            <Navbar />
-        </div>
+        <Router>
+            <div className="app">
+                <Navbar />
+                <Home />
+                {/* <Routes>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/posts" component={Posts} />
+                </Routes> */}
+            </div>
+        </Router>
     );
 }
 
